6f5e8c99208753b68f853387e6e78a6888571153
Steven King <nfstevenking@gmail.com>
2019-05-15 19:47:11 -0400

    Updated email address, links, and _blank

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  projects = [
+    {
+      title: 'WordCountTool.com', url: 'https://wordcounttool.com', asset: 'assets/WordCountTool.jpg',
+      description: 'A serverless Angular 6 web application that calculates the number of words in a textarea field, frequency of each word, and can manipulate the case of each word. Uses the @ng-toolkit/universal project to pre-render HTML to improve SEO. Traffic is about 34,000 pageviews a month.'
+    },
+    {
+      title: 'Overwatch.guide', url: 'https://overwatch.guide', asset: 'assets/OverwatchGuide.jpg',
+      description: 'Made in WordPress but using a custom made template, it\'s a guide for Blizard\'s videogame, Overwatch. Continuously updated over the last 2 years, it receives ~5500 unique visitors a month according to Google Analytics. Also shows off my (lack thereof) photoshop skills!'
+    }
+  ]
+
+  otherProjects = [
+    { title: 'Experient Groups Website', url: 'http://experientgroup.com/', role: 'Development, UX', company: 'Experient Group', year: 2016 }
+  ]
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  websites = [
+    { name: 'Email', url: 'mailto:career@stevenking.net' },
+    { name: 'Resume', url: 'https://github.com/StevenKing-Profile/Resume/blob/master/2019_Steven_King_Resume.pdf'},
+    { name: 'Github', url: 'https://github.com/StevenKing-Profile' },
+    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/steven-king-832566186/' }
+  ];
 
   constructor() { }
 
